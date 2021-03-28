f5e00c6b0c09edc0d902e9c0990d631278c3439a
Jaini Shah <jainishah1209@gmail.com>
2021-03-28 06:13:42 +0000

    added insert statement to host_info file and excuted through psql CLI tool

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -14,12 +14,19 @@ fi
 
 #parsing host hardware specifications
 hostname=$(hostname -f)
-cpu_number=$(lscpu | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
-cpu_architecture=$(lscpu | egrep "^Architecture:" | awk '{print $2}' | xargs)
-cpu_model=$(lscpu | egrep "^Model:" | awk '{print $2}' | xargs)
-cpu_mhz=$(lscpu | egrep "^CPU\sMHz:" | awk '{print $3}' | xargs)
-l2_cache=$(lscpu | egrep "^L2\scache:" | awk '{print $3}' | xargs)
+lscpu_out=`lscpu`
+cpu_number=$(echo "$lscpu_out" | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
+cpu_architecture=$(echo "$lscpu_out" | egrep "^Architecture:" | awk '{print $2}' | xargs)
+cpu_model=$(echo "$lscpu_out" | egrep "^Model:" | awk '{print $2}' | xargs)
+cpu_mhz=$(echo "$lscpu_out" | egrep "^CPU\sMHz:" | awk '{print $3}' | xargs)
+l2_cache=$(echo "$lscpu_out" | egrep "^L2\scache:" | awk '{print $3}' | xargs)
 total_mem=$(cat proc/meminfo | egrep "MemTotal:" | awk '{print $2}' | xargs)
 timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+
+#Query to insert data into host_info table
+insert_stmnt="INSERT INTO host_info (hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, total_mem, timestamp);"
+
+#Executing Insert statement through psql CLI tool
+psql -h localhost -U postgres -d host_agent -c "$insert_stmnt"
 
 exit 0
