d60b0d0e3130ea343f37aef9a2334a5f24730483
Jaini Shah <jainishah1209@gmail.com>
2021-03-29 19:37:23 +0000

    completed part 1 of script-parsing values

--- linux_sql/scripts/host_usage.sh
+++ linux_sql/scripts/host_usage.sh
@@ -15,5 +15,11 @@ fi
 memory_free=$(vmstat --unit M |awk '{for(i=NF;i>0;i--)if($i=="free"){x=i;break}}END{print $x}' | xargs)
 cpu_idle=$(vmstat --unit M |awk '{for(i=NF;i>0;i--)if($i=="id"){x=i;break}}END{print $x}' | xargs)
 cpu_kernel=$(vmstat --unit M |awk '{for(i=NF;i>0;i--)if($i=="sy"){x=i;break}}END{print $x}' | xargs)
-disk_io=
+disk_io=(vmstat -d |awk '{for(i=NF;i>0;i--)if($i=="cur"){x=i+1;break}}END{print $x}' | xargs)
 disk_available=$(df -BM / |awk '{for(i=NF;i>0;i--)if($i=="Available"){x=i;break}}END{print $x}' | xargs)
+timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+#Query to insert data into host_info table
+insert_stmnt="INSERT INTO host_usage(timestamp, , cpu_architecture, cpu_model, cpu_mhz, l2_cache, total_mem, timestamp);"
+
+#Executing Insert statement through psql CLI tool
+psql -h localhost -U postgres -d host_agent -c "$insert_stmnt"
