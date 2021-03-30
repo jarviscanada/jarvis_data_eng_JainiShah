757b2da0da07d31aac9deea9034ebfff0612633d
Jaini Shah <jainishah1209@gmail.com>
2021-03-30 04:26:36 +0000

    change data type of l2_cache to Varchar

--- linux_sql/sql/ddl.sql
+++ linux_sql/sql/ddl.sql
@@ -8,9 +8,9 @@ CREATE TABLE iF NOT EXISTS public.host_info
 	hostname VARCHAR NOT NULL UNIQUE,
 	cpu_number INT NOT NULL,
 	cpu_architecture VARCHAR NOT NULL,
-	cpu_model INT NOT NULLß,
-	cpu_mhz  VARCHAR NOT NULL,
-	L2_cache INT NOT NULL,
+	cpu_model INT NOT NULL,
+	cpu_mhz  INT NOT NULL,
+	L2_cache VARCHAR NOT NULL,
 	total_mem INT NOT NULL,
 	"timestamp" TIMESTAMP NOT NULL
 );
