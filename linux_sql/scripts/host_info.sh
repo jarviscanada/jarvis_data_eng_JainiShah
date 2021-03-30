41c35cceb833223b977d0771cb22f7e74b6dfe44
Jaini Shah <jainishah1209@gmail.com>
2021-03-30 04:24:27 +0000

    Change data type of l2_cache

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -20,11 +20,11 @@ cpu_architecture=$(echo "$lscpu_out" | egrep "^Architecture:" | awk '{print $2}'
 cpu_model=$(echo "$lscpu_out" | egrep "^Model:" | awk '{print $2}' | xargs)
 cpu_mhz=$(echo "$lscpu_out" | egrep "^CPU\sMHz:" | awk '{print $3}' | xargs)
 l2_cache=$(echo "$lscpu_out" | egrep "^L2\scache:" | awk '{print $3}' | xargs)
-total_mem=$(cat proc/meminfo | egrep "MemTotal:" | awk '{print $2}' | xargs)
+total_mem=$(cat /proc/meminfo | egrep "MemTotal:" | awk '{print $2}' | xargs)
 timestamp=$(date +"%Y-%m-%d %H:%M:%S")
 
 #Query to insert data into host_info table
-insert_stmnt="INSERT INTO host_info (hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, total_mem, timestamp) VALUES('$hostname', $cpu_number, '$cpu_architecture', $cpu_model, '$cpu_mhz', $l2_cache, $total_mem, '$timestamp');"
+insert_stmnt="INSERT INTO host_info (hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, total_mem, timestamp) VALUES('$hostname', $cpu_number, '$cpu_architecture', $cpu_model, $cpu_mhz, '$l2_cache', $total_mem, '$timestamp');"
 
 #Executing Insert statement through psql CLI tool
 psql -h localhost -U postgres -d host_agent -c "$insert_stmnt"
