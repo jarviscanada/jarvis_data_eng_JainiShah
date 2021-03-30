af2c8754ff9f2dc6744219b1725b438abc778d49
Jaini Shah <jainishah1209@gmail.com>
2021-03-30 18:24:57 +0000

    Final code for Monitoring Agent with crontab

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -19,7 +19,8 @@ cpu_number=$(echo "$lscpu_out" | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
 cpu_architecture=$(echo "$lscpu_out" | egrep "^Architecture:" | awk '{print $2}' | xargs)
 cpu_model=$(echo "$lscpu_out" | egrep "^Model:" | awk '{print $2}' | xargs)
 cpu_mhz=$(echo "$lscpu_out" | egrep "^CPU\sMHz:" | awk '{print $3}' | xargs)
-l2_cache=$(echo "$lscpu_out" | egrep "^L2\scache:" | awk '{print $3}' | xargs)
+l2_cache1=$(echo "$lscpu_out" | egrep "^L2\scache:" | awk '{print $3}' | xargs)
+l2_cache=${l2_cache1//[^[:digit:].-]/}
 total_mem=$(cat /proc/meminfo | egrep "MemTotal:" | awk '{print $2}' | xargs)
 timestamp=$(date +"%Y-%m-%d %H:%M:%S")
 
